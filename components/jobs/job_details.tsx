ab62fecbcef43475f00b96ae896bf5e9fa74a730
paola-jobvite <paola.lera@jobvite-inc.com>
2024-02-15 20:03:18 +0100

    removed unused code

--- components/jobs/job_details.tsx
+++ components/jobs/job_details.tsx
@@ -1,13 +1,4 @@
-import React, {useEffect} from "react";
-
 const JobDetails = ({ job }) => {
-  // useEffect(() => {
-  //   const slug = 'od00t90uXQkgIhffTVllz'
-  //   const jobId = 4; // Extract the second parameter from the URL
-  //   fetch(`https://connect.app.jvidev.localhost/endpoint/${slug}/job_details/${jobId}`).then(data => {
-  //     console.log(data)
-  //   });
-  //}, []);
   return (
     <div>
       <strong>Job Details</strong>
